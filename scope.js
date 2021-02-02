60bb6ccd6bd31b0226581327b41cbcb329f4e295
Istiaqe Ahmed Khan <istiaqeahmed.cse@gmail.com>
2021-02-03 02:18:41 +0600

    hosting

---
+++ scope.js
@@ -0,0 +1,13 @@
+let bonus = 20;
+
+function sum(first, second){
+    let result = first + second + bonus;
+    //console.log(bonus);
+    return result;
+}
+const output = sum(3, 7);
+console.log(bonus);
+console.log(output);
+
+//hostiing (that js dude article)
+// var diye variable declare korle uporer thekeo access korter pare but let, const access korte pare na error dekhabe . let, const scope function er vitorei simaboddo
